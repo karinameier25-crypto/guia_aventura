745600a7e6e691258be0755d6136b5e220b4a1ff
Karina <karinameier25@gmail.com>
2026-09-15 14:03:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'guia_aventura',
+  webDir: 'dist'
+};
+
+export default config;
